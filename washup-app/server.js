5c682b4098f8c014cc6e1593e1e0665581ebd320
mbferreiro <bferreiro@correo.um.edu.uy>
2023-09-24 09:57:04 -0300

    cambio

--- washup-app/server.js
+++ washup-app/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));// port en el que corre capaz que hay que cambiarlo
+app.listen(5432, () => console.log("Server Running"));// port en el que corre capaz que hay que cambiarlo
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
@@ -29,19 +29,15 @@ contactEmail.verify((error) => {
 });
 
 //send data haciendo post request
-router.post("/contact", (req, res) => {
+router.post("/user", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
   const mail = {
     from: name,
     to: "********@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
-           <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
-           <p>Message: ${message}</p>`,
+           <p>Email: ${email}</p>`,
   };
   contactEmail.sendMail(mail, (error) => {
     if (error) {

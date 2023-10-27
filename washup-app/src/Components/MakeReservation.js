e129be3011ef8ce81e85be66fd4575261da4d379
mbferreiro <bferreiro@correo.um.edu.uy>
2023-10-27 14:03:32 -0300

    cambios reserva

--- washup-app/src/Components/MakeReservation.js
+++ washup-app/src/Components/MakeReservation.js
@@ -6,9 +6,10 @@ import 'react-datepicker/dist/react-datepicker.css';
 const Reservations = () => {
   const [servicio, setServicio] = useState('');
   const [fecha, setFecha] = useState(null);
-  const [horario, setHorario] = useState('');
+  const [horarioSeleccionado, setHorarioSeleccionado] = useState('');
   const [ubicacion, setUbicacion] = useState('');
   const [fechasNoDisponibles, setFechasNoDisponibles] = useState([]);
+  const [horariosDisponibles, setHorariosDisponibles] = useState([]);
 
   const servicios = ['Lavado Exterior', 'Lavado Interior', 'Lavado Completo', 'Lavado de Motor', 'Lavado de Tapicería', 'Encerado', 'Limpieza de Vidrios', 'Desinfección', 'Lavado Express'];
   const horarios = ['9:00 AM', '10:00 AM', '11:00 AM', '2:00 PM', '3:00 PM'];
@@ -17,51 +18,53 @@ const Reservations = () => {
     const nuevoServicio = e.target.value;
     setServicio(nuevoServicio);
     setFecha(null);
-    setHorario('');
+    setHorarioSeleccionado('');
     // Realiza una solicitud al servidor para obtener las fechas no disponibles para el nuevo servicio
-    // URL del servidor para obtener fechas no disponibles (ajusta la URL según tu configuración)
     const fechasNoDisponiblesURL = `http://localhost:4000/fechasdisponibles/${nuevoServicio}`;
 
-    console.log('Haciendo solicitud GET a:', fechasNoDisponiblesURL);
-
-    fetch(fechasNoDisponiblesURL, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
+    fetch(fechasNoDisponiblesURL)
       .then((response) => {
         if (response.ok) {
-          console.log('Petición exitosa');
           return response.json(); // Parsea la respuesta JSON
         } else {
           console.error('Error al realizar la petición');
-          // Maneja errores si la solicitud no es exitosa
           throw new Error('Error en la solicitud');
         }
       })
       .then((data) => {
-        console.log('Datos de fechas no disponibles:', data.fechas_no_disponibles);
-        // Actualiza el estado con las fechas no disponibles
         setFechasNoDisponibles(data.fechas_no_disponibles);
       })
       .catch((error) => {
         console.error('Error:', error);
       });
-
   };
 
-  const handleFechaChange = (date) => {
-    setFecha(date);
+  const cargarHorariosDisponibles = (fechaSeleccionada) => {
+    // Realiza una solicitud al servidor para obtener los horarios disponibles
+    const horariosDisponiblesURL = `http://localhost:4000/horariosdisponibles?service=${servicio}&date=${fechaSeleccionada}`;
+
+    fetch(horariosDisponiblesURL)
+      .then((response) => {
+        if (response.ok) {
+          return response.json(); // Parsea la respuesta JSON
+        } else {
+          console.error('Error al obtener horarios disponibles');
+          throw Error('Error en la solicitud');
+        }
+      })
+      .then((data) => {
+        setHorariosDisponibles(data.horarios);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
 
-  const handleHorarioChange = (e) => {
-    setHorario(e.target.value);
-  };
-
-  const handleUbicacionChange = (e) => {
-    setUbicacion(e.target.value);
-  };
+  useEffect(() => {
+    if (fecha) {
+      cargarHorariosDisponibles(fecha.toISOString().slice(0, 10));
+    }
+  }, [fecha, servicio]);
 
   const backendURL = 'http://localhost:4000/reservations';
 
@@ -69,7 +72,7 @@ const Reservations = () => {
     const reservaData = {
       servicio,
       fecha,
-      horario,
+      horario: horarioSeleccionado, // Utilizamos el horario seleccionado
       ubicacion,
     };
 
@@ -93,19 +96,11 @@ const Reservations = () => {
 
     setServicio('');
     setFecha(null);
-    setHorario('');
+    setHorarioSeleccionado(''); // Limpiamos el horario seleccionado
     setUbicacion('');
   };
 
   const fechasNoDisponiblesFormatted = fechasNoDisponibles ? fechasNoDisponibles.map((fecha) => new Date(fecha)) : [];
-  
-  //const arrayDeFechas = ["2023-10-26", "2023-11-15", "2023-12-03"]; // Ejemplo de un array de fechas
-
-  //const fechasNoDisponiblesFormatted = arrayDeFechas.map((fecha) => new Date(fecha));
-
-// Ahora, fechasNoDisponiblesFormatted contendrá un array de objetos Date.
-
-
 
   return (
     <div className="products-container" style={{ marginTop: '40px' }}>
@@ -128,18 +123,18 @@ const Reservations = () => {
               <label>Selecciona una fecha:</label>
               <DatePicker
                 selected={fecha}
-                onChange={handleFechaChange}
+                onChange={(date) => setFecha(date)}
                 dateFormat="P"
                 className="select"
-                excludeDates={fechasNoDisponiblesFormatted} // Bloquea fechas no disponibles
+                excludeDates={fechasNoDisponiblesFormatted}
               />
             </div>
             {fecha && (
               <div className="form-group">
                 <label>Selecciona un horario:</label>
-                <select className="select" value={horario} onChange={handleHorarioChange}>
+                <select className="select" value={horarioSeleccionado} onChange={(e) => setHorarioSeleccionado(e.target.value)}>
                   <option value="">Selecciona un horario</option>
-                  {horarios.map((h, index) => (
+                  {horariosDisponibles.map((h, index) => (
                     <option key={index} value={h}>
                       {h}
                     </option>
@@ -149,10 +144,10 @@ const Reservations = () => {
             )}
           </>
         )}
-        {horario && (
+        {horarioSeleccionado && (
           <div className="form-group">
             <label>Especifique una ubicación:</label>
-            <input className="input" type="text" value={ubicacion} onChange={handleUbicacionChange} />
+            <input className="input" type="text" value={ubicacion} onChange={(e) => setUbicacion(e.target.value)} />
           </div>
         )}
         {ubicacion && (
@@ -166,3 +161,5 @@ const Reservations = () => {
 };
 
 export default Reservations;
+
+

10a3d758e6f2563232b327c961ad76db74d1596b
constanzaMira <cmira@correo.um.edu.uy>
2023-11-05 13:17:49 -0300

    changes in proyects

--- washup-app/src/Components/Project.js
+++ washup-app/src/Components/Project.js
@@ -11,20 +11,48 @@ export const Project = () => {
   const projects = [
     {
       title: "Montevideo Labs",
-      description: "Design & Development",
       imgUrl: projImg1,
     },
     {
       title: "Itau",
-      description: "Design & Development",
       imgUrl: projImg2,
     },
     {
       title: "Antel",
-      description: "Design & Development",
       imgUrl: projImg3,
     },
   ];
+
+  const projects1 = [
+    {
+      title: "Ancap",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Disa",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Universidad de Montevideo",
+      imgUrl: projImg3,
+    },
+  ];
+
+  const projects2 = [
+    {
+      title: "Banco Santander",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Tienda Inglesa",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Disco",
+      imgUrl: projImg3,
+    },
+  ];
+
 
   return (
     <section className="project" id="projects">
@@ -34,18 +62,18 @@ export const Project = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>We worked with well known companies, they trust in us, why can't you?</p>
+                <h2>Proyectos</h2>
+                <p>Trabajamos con empresas reconocidas, confían en nosotros, ¿por qué no puedes hacerlo tú?</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      <Nav.Link eventKey="first">1</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      <Nav.Link eventKey="second">2</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link eventKey="third">3</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
@@ -57,20 +85,43 @@ export const Project = () => {
                               <ProjectCard
                                 key={index}
                                 {...project}
-                                />
+                              />
                             )
                           })
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>We worked with well known companies, they trust in us, why can't you?</p>
+                    <Tab.Pane eventKey="second">
+                      <Row>
+                        {
+                          projects1.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                              />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p></p>
+                      <Row>
+                        {
+                          projects2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                              />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
+
               </div>}
             </TrackVisibility>
           </Col>

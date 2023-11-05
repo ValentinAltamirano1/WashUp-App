a87616f15760138facb4e1fa4e94f53c9d621dc1
ValentinAltamirano1 <valtamirano@correo.um.edu.uy>
2023-11-05 17:14:38 -0300

    payments callbacks

--- washup-app/src/Components/PaymentFailure.js
+++ washup-app/src/Components/PaymentFailure.js
@@ -1,9 +1,20 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+
 const FailurePage = () => {
+    const navigate = useNavigate();
+
+    const volverAlHome = () => {
+        navigate('/makereservation');
+    };
+
     return (
-        <div>
-            <h2>Error en el pago</h2>
-            <p>No hemos podido procesar tu pago. Por favor, inténtalo de nuevo o utiliza otro método de pago.</p>
-            {/* Botón para reintentar o enlaces a otros métodos de pago */}
+        <div style={{ marginTop: '50px' }}>
+            <h1 style={{ color: '#2596be', fontSize: '700', fontWeight: 'bold', marginBottom: '15px', textAlign:"center" , textShadow: '0 0 10px rgba(16, 46, 74, 0.5)'}}>Error en el pago</h1>
+            <div className="reset-container">
+                <p>No hemos podido procesar tu pago. Por favor, inténtalo de nuevo o utiliza otro método de pago.</p>
+                <button style={{ marginLeft: '10px' }} onClick={volverAlHome}>Volver a intentar</button>
+            </div>
         </div>
     );
 };

1e2ca74e37ff7a72318d30d9a8069cae56fa6ff9
ValentinAltamirano1 <valtamirano@correo.um.edu.uy>
2023-11-06 03:13:21 -0300

    admin total profit

--- washup-app/src/Components/AdminComponents/Deposits.js
+++ washup-app/src/Components/AdminComponents/Deposits.js
@@ -1,44 +1,77 @@
 import Typography from '@mui/material/Typography';
 import Title from './Title';
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 export default function Deposits() {
   const [totalByYear, setTotalByYear] = useState(null);
   const [totalByMonth, setTotalByMonth] = useState(null);
 
   useEffect(() => {
-    // Realiza la primera petición GET para obtener la suma de las reservas por año
-    axios.get('/ruta/al/endpoint/ano') // Reemplaza '/ruta/al/endpoint/ano' con la URL correcta de tu backend para obtener la suma por año
-      .then((response) => {
-        setTotalByYear(response.data.totalByYear);
-      })
-      .catch((error) => {
-        console.error('Error al obtener el total por año: ', error);
-      });
-
-    // Realiza la segunda petición GET para obtener la suma de las reservas por mes
-    axios.get('/ruta/al/endpoint/mes') // Reemplaza '/ruta/al/endpoint/mes' con la URL correcta de tu backend para obtener la suma por mes
-      .then((response) => {
-        setTotalByMonth(response.data.totalByMonth);
-      })
-      .catch((error) => {
-        console.error('Error al obtener el total por mes: ', error);
-      });
+    fetchYearProfit();
+    fetchMonthProfit();
   }, []);
 
+  const fetchMonthProfit = async () => {
+    try {
+      const month0 = new Date().getMonth() + 1;
+      const month= month0.toString();
+      const year0 = new Date().getFullYear();
+      const year= year0.toString();
+      const response = await fetch(`http://localhost:4000/reservations/profit/${year}/${month}`, {
+        method: 'GET',
+        /* headers: {
+          'Authorization': `Bearer ${token}` // Include the token for authentication if needed
+        },*/
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setTotalByMonth(data);
+      } else {
+        console.error('Failed to fetch reservations data');
+      }
+    } catch (error) {
+      console.error('Error fetching reservations data:', error);
+    }
+  };
+
+  const fetchYearProfit = async () => {
+    try {
+      const year0 = new Date().getFullYear();
+      const year= year0.toString();
+      const response = await fetch(`http://localhost:4000/reservations/profit/${year}`, {
+        method: 'GET',
+        /*headers: {
+          'Authorization': `Bearer ${token}`,
+        },*/
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setTotalByYear(data);
+      } else {
+        console.error('Failed to fetch assigned reservations');
+      }
+    } catch (error) {
+      console.error('Error fetching assigned reservations:', error);
+    }
+  };
   return (
     <React.Fragment>
       <Title>Depósitos recientes</Title>
       {totalByYear !== null && (
-        <Typography component="p" variant="h4">
-          Total por año: ${totalByYear}
+        <Typography component="p" variant="h6">
+          Total por año: ${totalByYear && totalByYear.profit}
         </Typography>
       )}
       {totalByMonth !== null && (
-        <Typography component="p" variant="h4">
-          Total por mes: ${totalByMonth}
-        </Typography>
+        <div style={{ marginTop: "16px" }}>
+          <Typography component="p" variant="h6">
+            Total por mes: ${totalByMonth && totalByMonth.profit}
+          </Typography>
+        </div>
       )}
     </React.Fragment>
   );
